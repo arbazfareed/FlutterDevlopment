15d92d2087789b3bd159583c067bc196140c0d82
arbazfareed <arbazfareed786@gmail.com>
2024-10-12 10:27:23 +0500

    Added Short key words

---
+++ android/app/src/main/kotlin/com/example/flutterdevlopment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterdevlopment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+MYapp()
+  );
+}
+
+class  MYapp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+return MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title:Center(
+    child: Text('Flutter Devlopment')
+),
+backgroundColor: Colors.green,
+
+),
+
+)
+);
+
+  }
+}
+//stf   + enter
+//   alt+enter
+//  ctr+space
+//
